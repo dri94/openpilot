ea96ac3e881471b59668a28549da87cca7e0da71
sunnyhaibin <haibin.wen3@gmail.com>
2021-08-22 03:42:52 -0400

    Feature: Dynamic Lane Profile Change (DLPC)

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -172,6 +172,7 @@ static void ui_draw_vision_lane_lines(UIState *s) {
   // paint path
   ui_draw_line(s, scene.track_vertices, nullptr, &track_bg);
 ***/
+  }
 }
 
 // Draw all world space objects.
