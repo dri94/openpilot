e41866fc0adfc7e05df5eb86aeab122016371dad
sunnyhaibin <haibin.wen3@gmail.com>
2021-09-05 14:51:04 -0400

    Feature: Dev UI

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -260,7 +260,7 @@ static void ui_draw_vision_event(UIState *s) {
 static void ui_draw_vision_face(UIState *s) {
   const int radius = 80;
   const int center_x = radius + (bdr_is * 2) + 10;
-  const int center_y = s->fb_h - footer_h / 2 + 10;
+  const int center_y = s->fb_h - footer_h / 2 + 50;
   ui_draw_circle_image(s, center_x, center_y, radius, "driver_face", s->scene.dm_active);
 }
 
