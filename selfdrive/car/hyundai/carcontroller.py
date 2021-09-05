9c2a41197e8beb2b8bacf7061a030b54c1c80f83
sunnyhaibin <haibin.wen3@gmail.com>
2021-09-05 16:15:05 -0400

    Fix annoying alarm when SCC stops behind a car

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -91,7 +91,7 @@ class CarController():
       # send resume at a max freq of 10Hz
       if (frame - self.last_resume_frame) * DT_CTRL > 0.1:
         # send 25 messages at a time to increases the likelihood of resume being accepted
-        can_sends.extend([create_clu11(self.packer, frame, CS.clu11, Buttons.RES_ACCEL)] * 25)
+        can_sends.extend([create_clu11(self.packer, frame, CS.clu11, Buttons.RES_ACCEL)])
         self.last_resume_frame = frame
 
     # 20 Hz LFA MFA message
